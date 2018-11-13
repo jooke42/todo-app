76c1aad4a8c9829b8548935784413a04377660f2
amarteaux <alexandre.marteaux@outlook.fr>
2018-11-13 22:02:41 +0100

    corrected return final todo after post

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/todo-app'));
 
 // Start the app by listening on the default
 // Heroku port
